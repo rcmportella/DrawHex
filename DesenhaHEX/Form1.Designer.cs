7cecbab74b625025f576a1b8bc85aaeea6419540
rcmportella <rcmportella@hotmail.com>
2018-06-06 20:29:53 -0300

    Add files via upload

---
+++ DesenhaHEX/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DesenhaHEX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 253);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DesenhaHEX/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesenhaHEX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap flag = new Bitmap(128, 111);
+            Graphics flagGraphics = Graphics.FromImage(flag);
+            /*
+            int red = 0;
+            int white = 11;
+            while (white <= 100)
+            {
+                flagGraphics.FillRectangle(Brushes.Red, 0, red, 200, 10);
+                flagGraphics.FillRectangle(Brushes.White, 0, white, 200, 10);
+                red += 20;
+                white += 20;
+            }
+            */
+            flagGraphics.Clear(Color.Transparent);
+            Point pt1 = new Point(31, 0);
+            Point pt2 = new Point(95, 0);
+            flagGraphics.DrawLine(new Pen(Brushes.Red), pt1, pt2);
+            pt1 = pt2;
+            pt2.X = 127;
+            pt2.Y = 55;
+            flagGraphics.DrawLine(new Pen(Brushes.Red), pt1, pt2);
+            pt1 = pt2;
+            pt2.X = 95;
+            pt2.Y = 110;
+            flagGraphics.DrawLine(new Pen(Brushes.Red), pt1, pt2);
+            pt1 = pt2;
+            pt2.X = 31;
+            pt2.Y = 110;
+            flagGraphics.DrawLine(new Pen(Brushes.Red), pt1, pt2);
+            pt1 = pt2;
+            pt2.X = 0;
+            pt2.Y = 55;
+            flagGraphics.DrawLine(new Pen(Brushes.Red), pt1, pt2);
+            pt1 = pt2;
+            pt2.X = 31;
+            pt2.Y = 0;
+            flagGraphics.DrawLine(new Pen(Brushes.Red), pt1, pt2);
+            flag.Save("hexagon.png", System.Drawing.Imaging.ImageFormat.Png);
+            pictureBox1.Image = flag;
+        }
+    }
+}
